71c4f6f8f72c9bf4e322dd6d9f3f53fd545d5f10
7umulte <tumultecolore@yahoo.com>
2011-03-28 18:14:50 +0200

    Ajout des tests three.js

---
+++ Tests/hutte.js
@@ -0,0 +1,22 @@
+// Converted from: hutte.obj
+//  vertices: 23
+//  faces: 32 
+//  materials: 1
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_threejs_slim.py
+
+
+var model = {
+    'materials': [	{
+	"DbgColor" : 0xeeeeee,
+	"DbgIndex" : 0,
+	"DbgName" : "(null)"
+	}],
+
+    'buffers': 'hutte.bin',
+
+    'end': (new Date).getTime()
+    }
+    
+postMessage( model );
---
+++ Tests/hutte_tex.js
@@ -0,0 +1,30 @@
+// Converted from: hutte_tex.obj
+//  vertices: 15
+//  faces: 26 
+//  materials: 1
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_threejs_slim.py
+
+
+var model = {
+    'materials': [	{
+	"DbgColor" : 0xeeeeee,
+	"DbgIndex" : 0,
+	"DbgName" : "Material.001",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.64000000000000001, 0.64000000000000001, 0.64000000000000001],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"mapDiffuse" : "disturb.jpg",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 1.0
+	}],
+
+    'buffers': 'hutte_tex.bin',
+
+    'end': (new Date).getTime()
+    }
+    
+postMessage( model );
